7c5a11557445397ca5c31652d02c7ad467a48c19
Braxton Huggins <braxton@pagodabox.com>
2015-10-19 10:57:00 -0600

    add travis and simple tests

---
+++ tests/prepare_test.sh
@@ -0,0 +1,18 @@
+echo running tests for python
+UUID=$(cat /proc/sys/kernel/random/uuid)
+
+pass "unable to start the $VERSION container" docker run --privileged=true -d --name $UUID nanobox/build-python sleep 365d
+
+defer docker kill $UUID
+
+pass "create db dir for pkgsrc" docker exec $UUID mkdir -p /data/var/db
+
+pass "create dir for environment variables" docker exec $UUID mkdir -p /data/etc/env.d 
+
+pass "Failed to update pkgsrc" docker exec $UUID /data/bin/pkgin up -y
+
+pass "unable to create code folder" docker exec $UUID mkdir -p /opt/code
+
+pass "Failed to inject requirements file" docker exec $UUID touch /opt/code/requirements.txt
+
+pass "Failed to run prepare script" docker exec $UUID bash -c "cd /opt/engines/python/bin; PATH=/data/sbin:/data/bin:\$PATH ./prepare '$(payload default-prepare)'"
