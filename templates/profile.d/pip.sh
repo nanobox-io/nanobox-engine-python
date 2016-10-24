9a612c87dbfd4e4d12b6bcd54343b6a8700f0b91
Tyler Flint <tylerflint@gmail.com>
2016-10-24 16:34:54 -0600

    Remove conditional around pip cache creation

--- templates/profile.d/pip.sh
+++ templates/profile.d/pip.sh
@@ -1,7 +1,5 @@
 # -*- mode: bash; tab-width: 2; -*-
 # vim: ts=2 sw=2 ft=bash noet
 
-if [ ! -s ${HOME}/.cache/pip ]; then
-  mkdir -p ${HOME}/.cache/pip
-  ln -sf {{code_dir}}/.pip-cache ${HOME}/.cache/pip
-fi
+mkdir -p ${HOME}/.cache/pip
+ln -sf {{code_dir}}/.pip-cache ${HOME}/.cache/pip
