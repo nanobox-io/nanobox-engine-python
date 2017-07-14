ec0c58c4f6e89b58385211cb00063b767b19217c
Tyler Flint <tylerflint@gmail.com>
2017-07-14 11:37:41 -0600

    Export newly created env vars

--- files/profile.d/pip.sh
+++ files/profile.d/pip.sh
@@ -4,5 +4,5 @@
 # at /app/.nanobox/pip. We need to set PATH to include the bin dir
 # and PYTHONPATH to include the src dir
 
-PYTHONPATH=/app/.nanobox/pip/src:$PYTHONPATH
-PATH=/app/.nanobox/pip/bin:$PATH
+export PATH=/app/.nanobox/pip/bin:$PATH
+export PYTHONPATH=/app/.nanobox/pip/src:$PYTHONPATH
